beb4e342906b8e4c5772ecb01011c2b5ebdb9897
wezzen <dimariqqq@gmail.com>
2025-03-16 22:53:50 +0600

    chore: multiproject structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,13 @@
 rootProject.name = "Insight"
 
+include("controller")
+project(":controller").projectDir = file("modules/controller")
+
+include("model")
+project(":model").projectDir = file("modules/model")
+
+include("repository")
+project(":repository").projectDir = file("modules/repository")
+
+include("service")
+project(":service").projectDir = file("modules/service")
--- src/main/java/com/github/wezzen/Main.java
+++
@@ -1,7 +0,0 @@
-package com.github.wezzen;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
