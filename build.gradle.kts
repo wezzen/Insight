e250029b261f3b34bf551fdcef66297492bb34e1
wezzen <dimariqqq@gmail.com>
2025-03-16 19:44:27 +0600

    jacoco and errorprone

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("net.ltgt.errorprone") version "4.1.0"
+    jacoco
 }
 
 group = "com.github.wezzen"
@@ -9,11 +11,27 @@ repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    errorprone("com.google.errorprone:error_prone_core:2.36.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.required.set(false)
+    }
 }
