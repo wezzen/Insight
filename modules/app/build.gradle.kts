e0e4f8c4bd949590ad2a3accaa6cc57136f1d359
wezzen <dimariqqq@gmail.com>
2025-03-17 23:29:06 +0600

    chore: managing modules dependencies

--- modules/app/build.gradle.kts
+++ modules/app/build.gradle.kts
@@ -1,3 +1,11 @@
 dependencies {
+    implementation(project(":controller"))
+    implementation(project(":dto"))
+    implementation(project(":model"))
+    implementation(project(":repository"))
+    implementation(project(":service"))
+}
 
+tasks.withType<JavaCompile> {
+    options.compilerArgs.add("-parameters")
 }
