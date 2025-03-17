5b453f43b5e8466aff07c42f3d7581e377c24b46
wezzen <dimariqqq@gmail.com>
2025-03-17 23:19:20 +0600

    ref: renamed DemoApplication into InsightApplication

--- modules/app/src/main/java/com/github/wezzen/insight/InsightApplication.java
+++ modules/app/src/main/java/com/github/wezzen/insight/InsightApplication.java
@@ -1,11 +1,11 @@
-package com.github.wezzen.insight.model;
+package com.github.wezzen.insight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class InsightApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(InsightApplication.class, args);
     }
 }
