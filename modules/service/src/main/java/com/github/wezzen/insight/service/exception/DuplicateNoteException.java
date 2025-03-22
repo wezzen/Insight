df227e7eb83456bc256605f3c783d2bc0a3e2e6d
wezzen <dimariqqq@gmail.com>
2025-03-22 17:07:24 +0600

    refactor: remove unused DuplicateNoteException

--- modules/service/src/main/java/com/github/wezzen/insight/service/exception/DuplicateNoteException.java
+++
@@ -1,9 +0,0 @@
-package com.github.wezzen.insight.service.exception;
-
-public class DuplicateNoteException extends RuntimeException {
-
-    public DuplicateNoteException(String message) {
-        super(message);
-    }
-
-}
