b8e691dd6bbf2390d88983d00ccf1a3f0c90c005
wezzen <dimariqqq@gmail.com>
2025-03-23 19:21:42 +0600

    tests: enable all tests

--- modules/dto/src/test/java/com/github/wezzen/insight/dto/response/CategoryDTOTest.java
+++ modules/dto/src/test/java/com/github/wezzen/insight/dto/response/CategoryDTOTest.java
@@ -3,7 +3,6 @@ package com.github.wezzen.insight.dto.response;
 import jakarta.annotation.Nonnull;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class CategoryDTOTest {
@@ -15,7 +14,6 @@ class CategoryDTOTest {
     }
 
     @Test
-    @Disabled
     void equalsAndHashCodeTest() {
         EqualsVerifier.forClass(CategoryDTO.class)
                 .withIgnoredAnnotations(Nonnull.class)
