732e3029d85443b95192a5d70a922a82e2b387a1
wezzen <dimariqqq@gmail.com>
2025-03-23 18:46:42 +0600

    tests: testing test badges in README.md

--- modules/dto/src/test/java/com/github/wezzen/insight/dto/response/CategoryDTOTest.java
+++ modules/dto/src/test/java/com/github/wezzen/insight/dto/response/CategoryDTOTest.java
@@ -3,6 +3,7 @@ package com.github.wezzen.insight.dto.response;
 import jakarta.annotation.Nonnull;
 import nl.jqno.equalsverifier.EqualsVerifier;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class CategoryDTOTest {
@@ -10,10 +11,11 @@ class CategoryDTOTest {
     @Test
     void creatingTest() {
         final CategoryDTO dto = new CategoryDTO("TestName");
-        Assertions.assertEquals("TestName", dto.name);
+        Assertions.assertNotEquals("TestName", dto.name);
     }
 
     @Test
+    @Disabled
     void equalsAndHashCodeTest() {
         EqualsVerifier.forClass(CategoryDTO.class)
                 .withIgnoredAnnotations(Nonnull.class)
